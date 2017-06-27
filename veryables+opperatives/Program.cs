0480eca260588f28fe3b6d98476b13a1006215d7
Jay <isaacsdaddy42293@gmail.com>
2017-06-27 10:57:47 -0400

    setuprepo

---
+++ veryables+opperatives/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace veryables_opperatives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
