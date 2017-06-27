59b1ef6d79f35cdcff5f91f522df49a53a42fb56
Jay <isaacsdaddy42293@gmail.com>
2017-06-27 15:05:32 -0400

    added practice codes

--- veryables+opperatives/variables+opperatives.cs
+++ veryables+opperatives/variables+opperatives.cs
@@ -10,6 +10,41 @@ namespace veryables_opperatives
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Reservation");
+            string reserveName = Console.ReadLine();
+            Console.WriteLine("How many people are in your party");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            Console.WriteLine("Total Due=");
+
+            double amountEach = 13.53d;
+
+            double totalBill = numberOfPeople * amountEach;
+            Console.WriteLine(totalBill);
+            Console.WriteLine("Reservation: " + reserveName);
+            Console.WriteLine("Total Due: $" + totalBill);
+
+
+
+        
+
+
+
+
+            Console.WriteLine("Cups of sugar");
+            int cupsOfSugar= int.Parse(Console.ReadLine());
+            Console.WriteLine("How many cups of sugar do you have?");
+            int totalCupsLisaHas = int.Parse(Console.ReadLine());
+            int totalCupsNeeded = cupsOfSugar - totalCupsLisaHas;
+            Console.WriteLine(totalCupsNeeded);
+            Console.WriteLine("Cups of sugar");
+
+
+
+
+
+
+
+    
         }
     }
 }
